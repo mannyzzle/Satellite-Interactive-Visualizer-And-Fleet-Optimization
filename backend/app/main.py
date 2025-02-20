df56320da40b751cfe1c1b4899a0a63b200d59ce
Enmanuel Hernandez <112276581+mannyzzle@users.noreply.github.com>
2025-02-19 21:00:12 -0800

    🚀 Fixed Docker deployment for Railway

--- backend/app/main.py
+++ backend/app/main.py
@@ -38,7 +38,7 @@ def startup_event():
         db_user = os.getenv('DB_USER')
         db_password = os.getenv('DB_PASSWORD')
         db_host = os.getenv('DB_HOST')
-        db_port = os.getenv('DB_PORT')
+        db_port = os.getenv('DB_PORT',"5432")
         db_name = os.getenv('DB_NAME')
 
         db_url = f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
