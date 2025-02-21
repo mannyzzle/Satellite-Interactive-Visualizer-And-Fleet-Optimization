240b49161ec763ffcd88d2cc763976bd20c47eda
Enmanuel Hernandez <112276581+mannyzzle@users.noreply.github.com>
2025-02-20 19:03:36 -0800

    Fix CORS for GitHub Pages

--- backend/app/main.py
+++ backend/app/main.py
@@ -10,7 +10,8 @@ app = FastAPI()
 # CORS (Frontend Compatibility)
 origins = [
     "http://localhost:5173",  # Vite default dev server
-    "http://127.0.0.1:5173",  # Alternative dev server
+    "http://127.0.0.1:5173",
+    "https://mannyzzle.github.io"  # Alternative dev server
 ]
 app.add_middleware(
     CORSMiddleware,
