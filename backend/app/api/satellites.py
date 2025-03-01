b1fe517c31f04fc27019f2164b74dbb3e50f7dd9
Enmanuel Hernandez <112276581+mannyzzle@users.noreply.github.com>
2025-02-28 17:47:45 -0800

    okay

--- backend/app/api/satellites.py
+++ backend/app/api/satellites.py
@@ -16,7 +16,7 @@ def sanitize_value(value):
 @router.get("/")
 def get_all_satellites(
     page: int = Query(1, ge=1),
-    limit: int = Query(10000, ge=1, le=10000),
+    limit: int = Query(1000, ge=1, le=11000),
     filter: str = Query(None)
 ):
     offset = (page - 1) * limit
